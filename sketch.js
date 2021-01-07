54929dc925755916305d2356d8074f76a3b33c71
Ragini1810 <74966906+Ragini1810@users.noreply.github.com>
2021-01-07 17:57:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,21 +12,21 @@ function setup(){
     canvas = createCanvas(800,600);
 
     block1 = createSprite(0,580,360,30);
-    block1.shapeColor = rgb(0,0,255);
+    block1.shapeColor = ("green");
 
     block2 = createSprite(295,580,200,30);
-    block2.shapeColor = rgb(255,128,0);
+    block2.shapeColor = ("blue");
 
     block3 = createSprite(515,580,200,30);
-    block3.shapeColor = rgb(153,0,76);
+    block3.shapeColor = ("yellow");
 
     block4 = createSprite(740,580,220,30);
-    block4.shapeColor = rgb(0,100,0);
+    block4.shapeColor = ("red");
 
     ball = createSprite(random(20,750),100, 40,40);
-    ball.shapeColor = rgb(255,255,255);
-    ball.velocityX = 4;
-    ball.velocityY = 9;
+    ball.shapeColor = ("white");
+    ball.velocityX = 6;
+    ball.velocityY = 10;
 
 }
 
@@ -36,23 +36,24 @@ function draw() {
     ball.bounceOff(edges);
 
     if(block1.isTouching(ball) && ball.bounceOff(block1)){
-        ball.shapeColor = rgb(0,0,255);
+        ball.shapeColor = ("green");
         music.play();
     }
 
     if(block2.isTouching(ball)){
-        ball.shapeColor = rgb(255,128,0);
-        ball.velocityX = 0;
-        ball.velocityY = 0;
-        music.stop();
+        ball.shapeColor = ("blue");
+
     }
 
     if(block3.isTouching(ball) && ball.bounceOff(block3)){
-        ball.shapeColor = rgb(153,0,76);
+        ball.shapeColor = ("yellow");
     }
 
     if(block4.isTouching(ball) && ball.bounceOff(block4)){
-        ball.shapeColor = rgb(0,100,0);
+        ball.shapeColor = ("red");
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+        music.stop();
     }
 
     drawSprites();
